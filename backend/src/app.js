7031e58331517ab280516f0f454ee19c4a1bf945
Deepanshu Sharma <deepanshusharma040@gmail.com>
2026-07-25 04:15:22 +0530

    npm run build

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,20 +3,25 @@ import cors from "cors";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Middlewares
 app.use(
   cors({
-    origin: "https://lead-hub-sandy.vercel.app",
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   }),
 );
 app.use(express.json());
 app.use(cookieParser());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 // Routes
 import leadRouter from "./routes/lead.routes.js";
@@ -24,6 +29,10 @@ import adminRouter from "./routes/admin.routes.js";
 
 app.use("/api/leads", leadRouter);
 app.use("/api/admin", adminRouter);
+
+app.use("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 app.use(errorHandler);
 
